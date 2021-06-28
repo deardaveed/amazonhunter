24c7fad2fce3d7e98e5b4485782f0d210e83b70a
David <daveman@gmail.com>
2021-06-27 21:14:11 -0700

    🪲 BUG FIX: fixed broken logo image link

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -15,19 +15,24 @@ function Homepage() {
 	}, [dispatch]);
 
 	return (
-		<div>
-			<img src={`../../../public/logo-orange-f8981d.png`} alt='logo' />
+		<div style={{ backgroundColor: '#e9ecef'}} >
+			<div>
+			<img src={process.env.PUBLIC_URL + "/logo-orange-f8981d.png"} alt="logo-orange"/>
+			<img src={process.env.PUBLIC_URL + "/logo-black.png"} alt="logo-black"/>
+			<img src={process.env.PUBLIC_URL + "/logo-white.png"} alt="logo-white"/>
+			{/* <img src="../../../%PUBLIC_URL%/logo-orange-f8981d.png" alt="logo"/> */}
+			</div>
       <table>
 				<tbody>
 					<tr>
 						{products.map((product) => {
 							return <div key={`${product.id}`}>{product.title}</div>
-						})};
+						})}
 					</tr>
         </tbody>
 			</table>
 		</div>
-	);
+	)
 };
 
 export default Homepage;
